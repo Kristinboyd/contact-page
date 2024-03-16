29d23d4e98e0e100db3d7840fe3ea5159a9e9048
Kristin Boyd <70916071+Kristinboyd@users.noreply.github.com>
2024-03-16 13:47:45 -0700

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+// Libraries 
+import React from "react";
+// Components
+import Card from "./Card";
+// Contacts
+import contacts from "../contacts";
+
+
+function App() {
+  return (
+  <div>
+    <h1 className="heading">My Contacts</h1>
+      <Card 
+        name={contacts[0].name} 
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+
+      <Card 
+        name={contacts[1].name} 
+        imgURL={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+
+      <Card 
+        name={contacts[2].name} 
+        imgURL={contacts[2].imgURL}
+        phone={contacts[2].phone}
+        email={contacts[2].email}
+      />
+  </div>
+
+  );
+}
+
+export default App;
